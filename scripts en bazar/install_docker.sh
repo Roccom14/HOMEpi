3f444257c843dd9ae4dbae00b7acf80f153acab7
Rocco Ronzano <40248252+Roccom14@users.noreply.github.com>
2024-02-05 19:17:26 +0100

    [ADD] Ajout de scripts
    - Ajout d'installation de Docker
    - Ajout de docker-compose
    - Modification du script de base du pi3

--- scripts en bazar/install_docker.sh
+++ scripts en bazar/install_docker.sh
@@ -1,24 +1,6 @@
-#!/bin/bash
-
-# Génération des locale en français suisse
-sudo locale-gen fr_CH.UTF-8
-sudo localectl set-locale LANG=fr_CH.UTF-8
-export LANGUAGE=fr_CH.UTF-8
-export LC_ALL=fr_CH.UTF-8
-export LANG=fr_CH.UTF-8
-export LC_CTYPE=fr_CH.UTF-8
-
-# Mise à jour du système
-sudo apt -y update && sudo apt -y full-upgrade
-
-# Mise à jour du firmware
-sudo rpi-update -y
-
-# Installation des programmes spécifiques au pi3
-sudo apt -y install certbot ca-certificates curl
-
-## Installation de Docker
 # Ajout de la clé GPG officielle de Docker
+sudo apt -y update
+sudo apt -y install ca-certificates curl
 sudo install -m 0755 -d /etc/apt/keyrings
 sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
 sudo chmod a+r /etc/apt/keyrings/docker.asc
@@ -30,5 +12,5 @@ sudo apt -y update
 # Installation des derniers paquets de Docker
 sudo apt -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
-# Redémarrage du système pour appliquer les paramètres
-sudo reboot now
+# Vérification de l'installation de Docker
+sudo docker run hello-world
