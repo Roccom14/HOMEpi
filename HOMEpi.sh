cb1b7eb6e3cfacb12ebaab134b247dd2c4106f47
Rocco Ronzano <40248252+Roccom14@users.noreply.github.com>
2024-02-07 19:42:48 +0100

    [UPD] Fichiers Docker
    Réorganisation et mise à jour des fichiers pour Docker

--- HOMEpi.sh
+++ HOMEpi.sh
@@ -16,32 +16,42 @@ fi
 
 # Fonction pour effectuer l'action 1
 action1() {
-    ./scripts/install_pi1.sh
+    ansible-playbook -i ./ansible/inventory.yml ./ansible/playbook_install_pi1.yml
 }
 
 # Fonction pour effectuer l'action 2
 action2() {
-    ./scripts/install_pi2.sh
+    ansible-playbook -i ./ansible/inventory.yml ./ansible/playbook_install_pi2.yml
 }
 
 # Fonction pour effectuer l'action 3
 action3() {
-    ./scripts/install_pi3.sh
+    ansible-playbook -i ./ansible/inventory.yml ./ansible/playbook_install_pi3.yml
 }
 
 # Fonction pour effectuer l'action 4
 action4() {
-    
+    ansible-playbook -i ./ansible/inventory.yml ./ansible/playbook_update.yml
 }
 
 # Fonction pour effectuer l'action 5
 action5() {
-    
+    ansible-playbook -i ./ansible/inventory.yml ./ansible/playbook_cups.yml
 }
 
 # Fonction pour effectuer l'action 6
 action6() {
-    
+    ansible-playbook -i ./ansible/inventory.yml ./ansible/playbook_nas.yml
+}
+
+# Fonction pour effectuer l'action 7
+action7() {
+    ansible-playbook -i ./ansible/inventory.yml ./ansible/playbook_rsync.yml
+}
+
+# Fonction pour effectuer l'action 8
+action8() {
+    ansible-playbook -i ./ansible/inventory.yml ./ansible/playbook_docker.yml
 }
 
 # Fonction pour afficher le menu
@@ -51,10 +61,12 @@ afficher_menu() {
     echo "1. Installer Pi1"
     echo "2. Installer Pi2"
     echo "3. Installer Pi3"
-    echo "4. Configurer Pi1"
-    echo "5. Configurer Pi2"
-    echo "6. Configurer Pi3"
-    echo -e "${RED}4. Quitter${CO}"
+    echo "4. Mettre à jour tout les Raspberry Pi"
+    echo "5. Mettre en place CUPS"
+    echo "6. Mettre en place le NAS"
+    echo "7. Mettre en place Rsync"
+    echo "8. Mettre en plce Docker"
+    echo -e "${RED}9. Quitter${CO}"
 }
 
 # Boucle principale
@@ -62,7 +74,7 @@ while true; do
     echo
     afficher_menu
     echo
-    read -p "Choisissez une option (1-4) : " choix
+    read -p "Choisissez une option (1-9) : " choix
     echo
 
     case $choix in
@@ -72,7 +84,9 @@ while true; do
         4) action4 ;;
         5) action5 ;;
         6) action6 ;;
-        7) echo -e "${ITALIC}Au revoir !${CO}"; echo ; exit ;;
-        *) echo -e "${RED}Option invalide, veuillez choisir une option valide (1-4).${CO}";;
+        7) action7 ;;
+        8) action8 ;;
+        9) echo -e "${ITALIC}Au revoir !${CO}"; echo ; exit ;;
+        *) echo -e "${RED}Option invalide, veuillez choisir une option valide (1-9).${CO}";;
     esac
 done
