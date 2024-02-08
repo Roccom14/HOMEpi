b37738edde713b5b3a2fa8f6d9de23f6503f907e
Rocco Ronzano <40248252+Roccom14@users.noreply.github.com>
2024-02-08 18:12:01 +0100

    [UPD] Mise à jour de HOMEpi.sh
    Mise à jour des options avec l'ajout d'une installation multiple pour tous les Pi

--- HOMEpi.sh
+++ HOMEpi.sh
@@ -16,41 +16,46 @@ fi
 
 # Fonction pour effectuer l'action 1
 action1() {
-    ansible-playbook -i ./ansible/inventory.yml ./ansible/playbook_install_pi1.yml
+    ansible-playbook -i ./ansible/inventory.yml ./ansible/master_playbook_install_pi.yml
 }
 
 # Fonction pour effectuer l'action 2
 action2() {
-    ansible-playbook -i ./ansible/inventory.yml ./ansible/playbook_install_pi2.yml
+    ansible-playbook -i ./ansible/inventory.yml ./ansible/playbook_install_pi1.yml
 }
 
 # Fonction pour effectuer l'action 3
 action3() {
-    ansible-playbook -i ./ansible/inventory.yml ./ansible/playbook_install_pi3.yml
+    ansible-playbook -i ./ansible/inventory.yml ./ansible/playbook_install_pi2.yml
 }
 
 # Fonction pour effectuer l'action 4
 action4() {
-    ansible-playbook -i ./ansible/inventory.yml ./ansible/playbook_update.yml
+    ansible-playbook -i ./ansible/inventory.yml ./ansible/playbook_install_pi3.yml
 }
 
 # Fonction pour effectuer l'action 5
 action5() {
-    ansible-playbook -i ./ansible/inventory.yml ./ansible/playbook_cups.yml
+    ansible-playbook -i ./ansible/inventory.yml ./ansible/playbook_update.yml
 }
 
 # Fonction pour effectuer l'action 6
 action6() {
-    ansible-playbook -i ./ansible/inventory.yml ./ansible/playbook_nas.yml
+    ansible-playbook -i ./ansible/inventory.yml ./ansible/playbook_cups.yml
 }
 
 # Fonction pour effectuer l'action 7
 action7() {
-    ansible-playbook -i ./ansible/inventory.yml ./ansible/playbook_rsync.yml
+    ansible-playbook -i ./ansible/inventory.yml ./ansible/playbook_nas.yml
 }
 
 # Fonction pour effectuer l'action 8
 action8() {
+    ansible-playbook -i ./ansible/inventory.yml ./ansible/playbook_rsync.yml
+}
+
+# Fonction pour effectuer l'action 9
+action9() {
     ansible-playbook -i ./ansible/inventory.yml ./ansible/playbook_docker.yml
 }
 
@@ -58,15 +63,16 @@ action8() {
 afficher_menu() {
     echo "Menu :"
     echo
-    echo "1. Installer Pi1"
-    echo "2. Installer Pi2"
-    echo "3. Installer Pi3"
-    echo "4. Mettre à jour tout les Raspberry Pi"
-    echo "5. Mettre en place CUPS"
-    echo "6. Mettre en place le NAS"
-    echo "7. Mettre en place Rsync"
-    echo "8. Mettre en plce Docker"
-    echo -e "${RED}9. Quitter${CO}"
+    echo "1. Installer tous les Pi"
+    echo "2. Installer Pi1"
+    echo "3. Installer Pi2"
+    echo "4. Installer Pi3"
+    echo "5. Mettre à jour tout les Raspberry Pi"
+    echo "6. Mettre en place CUPS"
+    echo "7. Mettre en place le NAS"
+    echo "8. Mettre en place Rsync"
+    echo "9. Mettre en place Docker"
+    echo -e "${RED}10. Quitter${CO}"
 }
 
 # Boucle principale
@@ -74,7 +80,7 @@ while true; do
     echo
     afficher_menu
     echo
-    read -p "Choisissez une option (1-9) : " choix
+    read -p "Choisissez une option (1-10) : " choix
     echo
 
     case $choix in
@@ -86,7 +92,8 @@ while true; do
         6) action6 ;;
         7) action7 ;;
         8) action8 ;;
-        9) echo -e "${ITALIC}Au revoir !${CO}"; echo ; exit ;;
-        *) echo -e "${RED}Option invalide, veuillez choisir une option valide (1-9).${CO}";;
+        9) action9 ;;
+        10) echo -e "${ITALIC}Au revoir !${CO}"; echo ; exit ;;
+        *) echo -e "${RED}Option invalide, veuillez choisir une option valide (1-10).${CO}";;
     esac
 done
